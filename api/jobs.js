b24bd5770c5815ae8999e1c6186b80a87b54cd0b
morsh <mor.shemesh@gmail.com>
2016-09-14 17:39:28 +0300

    update job push

--- api/jobs.js
+++ api/jobs.js
@@ -4,19 +4,43 @@ var express = require('express');
 var router = api.router = express.Router();
 var docRouter = require('docrouter').docRouter;
 var azure = require('azure-storage');
+var config = require('../config').svc;
 
 module.exports = api;
 
 docRouter(router, '/api/jobs', function (router) {
 
-  router.post('/push', function(req, res) {
+  router.post('/peek', function(req, res) {
+    
+    var queueSvc = azure.createQueueService(config.clusterStorageAccountName, config.clusterStorageAccountKey)
+        .withFilter(new azure.ExponentialRetryPolicyFilter());
 
-    var config = null;
-    try {
-      config = require('../lib/config');
-    } catch (e) {
-      return res.status(400).send('There was an error reading configuration:\n' + err);
-    }
+    queueSvc.createQueueIfNotExists(config.inputQueueName, function(err) {
+      if (err) {
+        return res.status(400).send('There was an error reading the queue:\n' + err);
+      }
+
+      queueSvc.peekMessage(config.inputQueueName, function(err, result, response){
+        if(err){
+          return res.status(400).send('There was a problem inserting message into queue:\n' + err);
+        }
+
+        console.log('message inserted to queue successfully');
+        res.json({ result: result });
+      });
+    });
+
+  },
+    {
+      id: 'jobs_peek',
+      name: 'peek',
+      usage: 'jobs peek',
+      example: 'jobs peek',
+      doc: 'Peeking message from queue',
+      params: { }
+    });
+
+  router.post('/push', function(req, res) {
 
     var data = req.body.data;
     if (!data) {
@@ -48,7 +72,15 @@ docRouter(router, '/api/jobs', function (router) {
       usage: 'jobs push',
       example: 'jobs push',
       doc: 'Pushing new jobs to the jobs queue',
-      params: { }
+      params: {
+        "data": {
+          "short": "d",
+          "type": "string",
+          "doc": "jobs push --data 'bla bla bla'",
+          "style": "body",
+          "required": false
+        }
+      }
     });
 
 });
